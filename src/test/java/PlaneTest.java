7a6027ef68800f234bef4df920d0787557c2af5e
Rebeka Geczi <geczirebeka@gmail.com>
2020-08-30 10:09:36 +0100

    plane tests

--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+import plane.Plane;
+import plane.PlaneType;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+
+    Plane plane;
+
+    @Before
+    public void before() {
+        plane = new Plane(PlaneType.BOEING737);
+    }
+
+    @Test
+    public void hasPlaneType() {
+        assertEquals(PlaneType.BOEING737, plane.getPlaneType());
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(270, plane.getCapacity());
+    }
 }
